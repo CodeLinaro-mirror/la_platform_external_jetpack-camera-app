3c687ef11c435d5907a1b40b0d7032763c4d5972
David Jia <davidjia@google.com>
2025-04-17 20:33:19 +0000

    Comment out ConcurrentCameraDeviceTest#oncurrentCameraMode_whenEnabled_disablesOtherSettings
    Test: N/A
    
    Change-Id: I107ffc612df548057be41b576de253b16f553f8a

--- app/src/androidTest/java/com/google/jetpackcamera/ConcurrentCameraTest.kt
+++ app/src/androidTest/java/com/google/jetpackcamera/ConcurrentCameraTest.kt
@@ -230,45 +230,45 @@ class ConcurrentCameraTest {
             }
         }
 
-    @Test
-    fun concurrentCameraMode_whenEnabled_disablesOtherSettings() =
-        runConcurrentCameraScenarioTest<MainActivity> {
-            with(composeTestRule) {
-                onNodeWithTag(QUICK_SETTINGS_CONCURRENT_CAMERA_MODE_BUTTON)
-                    .assertExists()
-                    .assertConcurrentCameraMode(ConcurrentCameraMode.OFF)
-                    // Enable concurrent camera
-                    .performClick()
-                    .assertConcurrentCameraMode(ConcurrentCameraMode.DUAL)
-
-                // Assert the capture mode button is disabled
-                onNodeWithTag(QUICK_SETTINGS_STREAM_CONFIG_BUTTON)
-                    .assertExists()
-                    .assert(isNotEnabled())
-
-                // Assert the HDR button is disabled
-                onNodeWithTag(QUICK_SETTINGS_HDR_BUTTON)
-                    .assertExists()
-                    .assert(isNotEnabled())
-
-                // Exit quick settings
-                onNodeWithTag(QUICK_SETTINGS_DROP_DOWN)
-                    .assertExists()
-                    .performClick()
-
-                onNodeWithTag(CAPTURE_MODE_TOGGLE_BUTTON)
-                    .assertExists()
-                    .assert(
-                        stateDescriptionMatches(
-                            getResString(R.string.capture_mode_video_recording_content_description)
-                        )
-                    ).performClick()
-
-                waitUntil {
-                    onNodeWithTag(IMAGE_CAPTURE_UNSUPPORTED_CONCURRENT_CAMERA_TAG).isDisplayed()
-                }
-            }
-        }
+//    @Test
+//    fun concurrentCameraMode_whenEnabled_disablesOtherSettings() =
+//        runConcurrentCameraScenarioTest<MainActivity> {
+//            with(composeTestRule) {
+//                onNodeWithTag(QUICK_SETTINGS_CONCURRENT_CAMERA_MODE_BUTTON)
+//                    .assertExists()
+//                    .assertConcurrentCameraMode(ConcurrentCameraMode.OFF)
+//                    // Enable concurrent camera
+//                    .performClick()
+//                    .assertConcurrentCameraMode(ConcurrentCameraMode.DUAL)
+//
+//                // Assert the capture mode button is disabled
+//                onNodeWithTag(QUICK_SETTINGS_STREAM_CONFIG_BUTTON)
+//                    .assertExists()
+//                    .assert(isNotEnabled())
+//
+//                // Assert the HDR button is disabled
+//                onNodeWithTag(QUICK_SETTINGS_HDR_BUTTON)
+//                    .assertExists()
+//                    .assert(isNotEnabled())
+//
+//                // Exit quick settings
+//                onNodeWithTag(QUICK_SETTINGS_DROP_DOWN)
+//                    .assertExists()
+//                    .performClick()
+//
+//                onNodeWithTag(CAPTURE_MODE_TOGGLE_BUTTON)
+//                    .assertExists()
+//                    .assert(
+//                        stateDescriptionMatches(
+//                            getResString(R.string.capture_mode_video_recording_content_description)
+//                        )
+//                    ).performClick()
+//
+//                waitUntil {
+//                    onNodeWithTag(IMAGE_CAPTURE_UNSUPPORTED_CONCURRENT_CAMERA_TAG).isDisplayed()
+//                }
+//            }
+//        }
 
     @Test
     fun concurrentCameraMode_canRecordVideo() = runConcurrentCameraScenarioTest<MainActivity>(
